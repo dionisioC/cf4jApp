214eac9bfc1678b157a666cd33b62a62cb3691b3
Dionisio Cortés Fernández <cortesfernandezdionisio@gmail.com>
2021-11-14 18:48:18 +0100

    Refactor to be more like jmeter

--- locust-image/locust-tasks/tasks.py
+++ locust-image/locust-tasks/tasks.py
@@ -3,35 +3,15 @@ from locust import HttpUser, task, between
 class MetricsTaskSet(HttpUser):
     wait_time = between(1, 2)
 
-    @task(1)
-    def adjusted_cosine(self):
-        self.client.get("/recommendations/itemknncomparison?algorithm=ADJUSTEDCOSINE&numNeighbors=10000&aggregationApproach=mean", name="ADJUSTEDCOSINE")
-
-    @task(2)
-    def correlation(self):
-        self.client.get("/recommendations/itemknncomparison?algorithm=CORRELATION&amp;numNeighbors=10000&amp;aggregationApproach=mean", name="CORRELATION")
-
-    @task(3)
-    def cosine(self):
-        self.client.get("/recommendations/itemknncomparison?algorithm=COSINE&amp;numNeighbors=10000&amp;aggregationApproach=mean", name="COSINE")
-
-    @task(4)
-    def jaccard(self):
-        self.client.get("/recommendations/itemknncomparison?algorithm=JACCARD&amp;numNeighbors=10000&amp;aggregationApproach=mean", name="JACCARD")
-
-    @task(5)
-    def jmsd(self):
-        self.client.get("/recommendations/itemknncomparison?algorithm=JMSD&amp;numNeighbors=10000&amp;aggregationApproach=mean", name="JMSD")
-
-    @task(6)
-    def msd(self):
-        self.client.get("/recommendations/itemknncomparison?algorithm=MSD&amp;numNeighbors=10000&amp;aggregationApproach=mean", name="MSD")
-
-    @task(7)
-    def singularities(self):
-        self.client.get("/recommendations/itemknncomparison?algorithm=SINGULARITIES&amp;numNeighbors=10000&amp;aggregationApproach=mean", name="SINGULARITIES")
-
-    @task(8)
-    def spearmanrank(self):
-        self.client.get("/recommendations/itemknncomparison?algorithm=SPEARMANRANK&amp;numNeighbors=10000&amp;aggregationApproach=mean", name="SPEARMANRANK")
+    @task
+    def item_knn_comparison(self):
+        for i in range(10):
+            self.client.get("/recommendations/itemknncomparison?algorithm=ADJUSTEDCOSINE&numNeighbors=10000&aggregationApproach=mean", name="ADJUSTEDCOSINE")
+            self.client.get("/recommendations/itemknncomparison?algorithm=CORRELATION&numNeighbors=10000&aggregationApproach=mean", name="CORRELATION")
+            self.client.get("/recommendations/itemknncomparison?algorithm=COSINE&numNeighbors=10000&aggregationApproach=mean", name="COSINE")
+            self.client.get("/recommendations/itemknncomparison?algorithm=JACCARD&numNeighbors=10000&aggregationApproach=mean", name="JACCARD")
+            self.client.get("/recommendations/itemknncomparison?algorithm=JMSD&numNeighbors=10000&aggregationApproach=mean", name="JMSD")
+            self.client.get("/recommendations/itemknncomparison?algorithm=MSD&numNeighbors=10000&aggregationApproach=mean", name="MSD")
+            self.client.get("/recommendations/itemknncomparison?algorithm=SINGULARITIES&numNeighbors=10000&aggregationApproach=mean", name="SINGULARITIES")
+            self.client.get("/recommendations/itemknncomparison?algorithm=SPEARMANRANK&numNeighbors=10000&aggregationApproach=mean", name="SPEARMANRANK")
 
